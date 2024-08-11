bdcb62920c9fc4a81d6dcc0b63a0e963ece8aa64
lovrezchaudhary <lovrezchaudhary@gmail.com>
2024-08-11 16:15:48 +0530

    Add components

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
+import {InputBox} from './components/input'
+import {useCurrencyInfo} from './hooks/useCurrencyInfo'
+
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+  
 
   return (
     <>
